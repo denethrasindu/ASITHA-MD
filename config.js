d397ad340e32e663ae385b498226b62e4486c9c7
denethrasindu <rasindunimsara717@gmail.com>
2024-11-03 17:05:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "zTY2xCST#9k6YFwatnaQqSmqCJE_KkpRvk_IzfGO0ry3gO7__iNE",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=KixXUJqb#3edt1AgGesft0lHo675G6QHGOYLi7FGR9evi2U9YYAs",
+MONGODB: process.env.MONGODB || "mongodb+srv://drn200901:5srGVc8qe2xBOMiV@cluster0.mbwpm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
